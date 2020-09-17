879d6edbf63f7a9553b7fd2f09348939546b5fe8
mudittiwari2000 <mudittiwari2000@gmail.com>
2020-09-18 00:07:43 +0530

    version 1.1.0

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
+const hamburger = document.querySelector('.hamburger');
+const hamButton = document.querySelector('.ham-btn');
+const primaryNav = document.querySelector('#primary-nav');
+
+hamButton.addEventListener('click', e => {
+  hamButton.classList.toggle('ham-close');
+  hamburger.classList.toggle('short');
+  primaryNav.classList.toggle('open');
+});
+
 function change(first, second, third) {
-    
-    // console.log(`first = ${first} \nsecond = ${second} \nthird = ${third}`);
-    first.style.display = 'none';
-    second.style.display = 'block';
-    third.style.display = 'block';
+
+  // console.log(`first = ${first} \nsecond = ${second} \nthird = ${third}`);
+  first.style.display = 'none';
+  second.style.display = 'block';
+  third.style.display = 'block';
 }
 
 function changeBack(first, second, third) {
-    
-    // console.log(`first = ${first} \nsecond = ${second} \nthird = ${third}`);
-    first.style.display = 'block';
-    second.style.display = 'none';
-    third.style.display = 'none';
+
+  // console.log(`first = ${first} \nsecond = ${second} \nthird = ${third}`);
+  first.style.display = 'block';
+  second.style.display = 'none';
+  third.style.display = 'none';
 }
 
-function photoGalleryFunc (first, second, third) {
-    
-    // console.log(`first = ${first} \nsecond = ${second} \nthird = ${third}`);
-    first.style.display = 'block';
-    second.style.display = 'none';
-    third.style.display = 'none';
+function photoGalleryFunc(first, second, third) {
 
-    let goBack = document.getElementById('go-back-btn');
-    if (goBack.style.display == 'none') goBack.style.display = 'inline-block';
+  // console.log(`first = ${first} \nsecond = ${second} \nthird = ${third}`);
+  first.style.display = 'block';
+  second.style.display = 'none';
+  third.style.display = 'none';
+
+  let goBack = document.getElementById('go-back-btn');
+  if (goBack.style.display == 'none') goBack.style.display = 'inline-block';
 }
